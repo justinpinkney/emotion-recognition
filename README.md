fa14e1a77ce55a733db88e60f690b28eb9ac610c
Justin <justinpinkney@gmail.com>
2018-05-23 17:07:53 +0100

    update data prep

---
+++ README.md
@@ -0,0 +1,3 @@
+- download ferPlus csv
+- download fer2013 csv
+- run fer2013Plusprep.m
--- fer2013Plusprep.m
+++ fer2013Plusprep.m
@@ -1,61 +1,52 @@
-dataRoot = '.';
+dataRoot = 'data';
 
-imageTable = readtable('fer2013.csv', 'Delimiter', ',');
-labels = readtable('fer2013new.csv');
+% TODO assert the data files are present
+
+imageTable = readtable(fullfile(dataRoot, 'fer2013.csv'), 'Delimiter', ',');
+labels = readtable(fullfile(dataRoot, 'fer2013new.csv'));
 emotions = categorical(labels.Properties.VariableNames(3:end));
 [~, idx] = max(labels{:, 3:end}, [], 2);
 label_data = table(categorical(labels.Usage), ...
                     labels.ImageName, ...
                     emotions(idx)', ...
                     'VariableNames', {'Dataset', 'filename', 'emotion'});
-                
-for emotion = emotions
-    if ~isfolder(char(emotion))
-        mkdir(char(emotion))
+
+for usage = unique(labels.Usage)'
+    stem = fullfile(dataRoot, usage{1});
+    for emotion = emotions
+        directory = fullfile(stem, char(emotion));
+        if ~isfolder(directory)
+            mkdir(directory);
+        end
     end
 end
 
 parfor iRow = 1:height(labels)
-    filename = labels.ImageName{iRow};
+    thisRow = labels(iRow, :);
+    thisImageRow = imageTable(iRow, :);
+    
+    filename = thisRow.ImageName{1};
     if isempty(filename)
         continue;
     end
-    [~, emotionIdx] = max(labels{iRow, 3:end});
+    [~, emotionIdx] = max(thisRow{1, 3:end});
     if emotionIdx >= 9
         continue;
     end
 
-    votes = labels{iRow, 3:end};
-    if votes(emotionIdx) < 5
+    votes = thisRow{1, 3:end};
+    % Remove uncertain ones (from paper)
+    if votes(emotionIdx) < 6
         continue;
     end
-    folderName = char(emotions(emotionIdx));
+    folderName = fullfile(dataRoot, ...
+                            thisRow.Usage{1}, ...
+                            char(emotions(emotionIdx)));
     
-    imageData = imageTable.pixels{iRow};
+    imageData = thisImageRow.pixels{1};
     im = str2double(split(imageData));
     im = uint8(reshape(im, 48, 48));
     im = im';
     disp(['saving ', filename])
     imwrite(im, fullfile(folderName, filename));
 end
-
-% label_data(strcmp(label_data.filename, ''), :) = [];
-% 
-% images = imageDatastore(dataRoot, 'IncludeSubfolders', true);
-%%
-% for iFile = 1:numel(images.Files)
-%     x = images.Files{iFile};
-%     if contains(x,'Training')
-%         continue
-%     end
-%     if ~strcmp(label_data.filename, getFilename(x))
-%         disp(x)
-%     else
-%         imshow(images.Files{iFile})
-%         title(char(label_data.emotion(strcmp(label_data.filename, getFilename(x)))))
-%         pause(1)
-%     end
-% end
-% relevant_labels = cellfun(@(x) label_data.emotion(strcmp(label_data.filename, getFilename(x))), ...
-%                             images.Files, 'UniformOutput', true);
-% images.Labels = label_data;
